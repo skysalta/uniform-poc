c333349714190f738f731d7fc8952e8bab9b8202
Gökce Demiral <gdemiral@saltagroup.com>
2022-11-30 15:13:13 +0100

    adding preview func

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  serverRuntimeConfig: {
+    uniform: {
+      apiKey: process.env.UNIFORM_API_KEY,
+      previewSecret: process.env.UNIFORM_PREVIEW_SECRET
+    }
+  },
+  publicRuntimeConfig: {
+    uniform: {
+      projectId: process.env.UNIFORM_PROJECT_ID
+    }
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
